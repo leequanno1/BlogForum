f06021a2db2ba51f83438f90f69f4c2818d63721
leequanno1 <binquan1234@gmail.com>
2024-05-11 21:59:54 +0700

    #4 - Quan add TagController.java file.

--- src/main/java/com/example/springdemo/apicontrollers/TagController.java
+++ src/main/java/com/example/springdemo/apicontrollers/TagController.java
@@ -13,6 +13,11 @@ import java.util.Map;
 @RestController
 public class TagController {
 
+    /**
+     * 1 - Get all tag
+     * This function return list of tag name.
+     * If there is no tag in this list return the status code: NOT_FOUND
+     * */
     @GetMapping("/api/tag/getall")
     public ResponseEntity getAll () {
         TagService tagService = new TagService();
@@ -23,6 +28,12 @@ public class TagController {
         return ResponseEntity.status(HttpStatus.OK).body(res);
     }
 
+    /**
+     * 2 - Get tags has same or similar name
+     * This function return list of tag has name similar to the provided name.
+     * If there is no tag in this list return the status code: NOT_FOUND
+     * @param request The request body contain 1 key: "name".
+     * */
     @GetMapping("/api/tag/getsimilar")
     public ResponseEntity getSimilar(@RequestParam Map<String,String> request) {
         String name = "";
@@ -37,6 +48,14 @@ public class TagController {
         return ResponseEntity.status(HttpStatus.OK).body(res);
     }
 
+    /**
+     * 3 - Add new tag
+     * This function handle add new tag.
+     * If the request body not contain all necessary keys then function return status code: BAD_REQUEST
+     * If user add new tag successful then return status code: OK.
+     * Otherwise, function return status code: NOT_ACCEPTABLE.
+     * @param request The request body contain 1 key: "name".
+     * */
     @PutMapping("/api/tag/add")
     public ResponseEntity addTag(@RequestBody Map<String, String> request) {
         if(!request.containsKey("name")) {
@@ -51,6 +70,14 @@ public class TagController {
         return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(StatusBodyMessageService.statusNotAcceptable());
     }
 
+    /**
+     * 4 - Update tag name
+     * This function handle update tag's name.
+     * If the request body not contain all necessary keys then function return status code: BAD_REQUEST
+     * If user add new tag successful then return status code: OK.
+     * Otherwise, function return status code: NOT_ACCEPTABLE.
+     * @param request The request body contain 1 key: "name".
+     * */
     @PutMapping("/api/tag/update")
     public ResponseEntity updateTag(@RequestBody Map<String, String> request) {
         if(!request.containsKey("currentName") || !request.containsKey("newName")) {
--- src/main/java/com/example/springdemo/services/TagService.java
+++ src/main/java/com/example/springdemo/services/TagService.java
@@ -9,23 +9,45 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * This is a Service class provide method to get API body.
+ * */
 public class TagService extends DatabaseService{
 
+    /**
+     * This is a SQL query to get all tags.
+     * */
     private static final String GET_ALL =       "SELECT * FROM [Tag]";
 
+    /**
+     * This is a SQL query to get all tags have name similar to the provide name.
+     * */
     private static final String GET_SIMILAR =   "SELECT * FROM [Tag]\n" +
                                                 "WHERE [Tag].TagName LIKE ?";
 
+    /**
+     * This is a SQL query to add new tag by provide new name.
+     * If new name already exit then doing nothing.
+     * */
     private static final String ADD_TAG =       "INSERT INTO [Tag] ([TagName])\n" +
                                                 "SELECT ?\n" +
                                                 "WHERE NOT EXISTS (\n" +
                                                 "    SELECT 1 FROM [Tag] WHERE [TagName] = ?\n" +
                                                 ");";
 
+    /**
+     * This is a SQL query to update current tag name to new name.
+     * If new name is already exit then don't change the current name.
+     * */
     private static final String UPDATE_TAG =    "UPDATE [Tag]\n" +
-                                                "SET [TagName] = ?\n" +
+                                                "SET [TagName] = CASE WHEN NOT EXISTS (SELECT 1 FROM [Tag] WHERE [TagName] = ?) THEN ?\n" +
+                                                "              ELSE [TagName]\n" +
+                                                "              END\n" +
                                                 "WHERE [TagName] = ?";
 
+    /**
+     * This function return a list of all tag's name.
+     * */
     public List<Object> getAll() {
         List<Object> res = new ArrayList<>();
         try (Connection connection = getDataSource().getConnection()) {
@@ -38,6 +60,10 @@ public class TagService extends DatabaseService{
         return res;
     }
 
+    /**
+     * This function return a list of all tag have name similar the provided name.
+     * @param name The provided name for searching purpose.
+     * */
     public List<Object> getSimilar(String name) {
         List<Object> res = new ArrayList<>();
         try (Connection connection = getDataSource().getConnection()) {
@@ -51,6 +77,11 @@ public class TagService extends DatabaseService{
         return res;
     }
 
+    /**
+     * This function add a new tag's name to database.
+     * Returning result if it inserted ok or not.
+     * @param name Tag's name
+     * */
     public boolean addNewTag(String name) {
         try (Connection connection = getDataSource().getConnection()) {
             try (PreparedStatement preparedStatement = connection.prepareStatement(ADD_TAG)){
@@ -67,11 +98,18 @@ public class TagService extends DatabaseService{
         return false;
     }
 
+    /**
+     * This function update name for a current tag by giving them a new name.
+     * Returning result if it updated ok or not.
+     * @param currentName The current tag's name
+     * @param newName The new tag's name
+     * */
     public boolean updateTag(String currentName , String newName) {
         try (Connection connection = getDataSource().getConnection()) {
             try (PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_TAG)){
                 preparedStatement.setString(1, newName);
-                preparedStatement.setString(2, currentName);
+                preparedStatement.setString(2, newName);
+                preparedStatement.setString(3, currentName);
                 int rowInserted = preparedStatement.executeUpdate();
                 if(rowInserted > 0) {
                     return true;
@@ -83,6 +121,11 @@ public class TagService extends DatabaseService{
         return false;
     }
 
+    /**
+     * This function help get all tag value form the query to the "res" object.
+     * @param res The result list.
+     * @param preparedStatement The statement contain the SQL query.
+     * */
     private void getTagList(List<Object> res, PreparedStatement preparedStatement) throws SQLException {
         ResultSet resultSet = preparedStatement.executeQuery();
         while (resultSet.next()) {
