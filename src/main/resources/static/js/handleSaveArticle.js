f79a14dceb5631da3725a171b01488cd3617a294
leequanno1 <binquan1234@gmail.com>
2024-05-24 20:33:44 +0700

    done add loading

--- src/main/resources/static/js/handleSaveArticle.js
+++ src/main/resources/static/js/handleSaveArticle.js
@@ -4,38 +4,72 @@ postArticle.addEventListener('click' , (e) => {
     let title = document.getElementById("articleTitle").value;
     let tagList = document.getElementById("tagList").value;
     let {editorContent, imgUrls} = handelArticleContentImageSrc(editor.getData());
-    console.log(title);
-    console.log(tagList);
-    console.log(editorContent);
-    console.log(imgUrls);
-    // send formData to server; @RequestParam("title") String title,
-    //                              @RequestParam("content") String content,
-    //                              @RequestParam("tags") String tags,
-    //                              @RequestParam("images") List<MultipartFile> images
-    // /api/article/new
-
-    fetch('/api/article/newbase64', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            title : title,
-            content : editorContent,
-            tags : tagList,
-            images : imgUrls
-        })
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+    if (!(validateTitle() && validateTag() && validateContent())) {
+        return;
+    }
+    loadingSpinner();
+    // fetch('/api/article/newbase64', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({
+    //         title : title,
+    //         content : editorContent,
+    //         tags : tagList,
+    //         images : imgUrls
+    //     })
+    // })
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data);
+    //     })
+    //     .catch(error => {
+    //         console.error('Error:', error);
+    //     });
 })
 
-let handelArticleContentImageSrc = function (editorData) {
+const loadingSpinner = function () {
+    postArticle.innerHTML = `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                Loading...`;
+    postArticle.disabled = true;
+}
+
+const doneLoading = function () {
+
+}
+
+const validateTitle = function () {
+    let title = document.getElementById("articleTitle").value;
+    if (title.trim().length > 0) {
+        // message
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const validateTag = function () {
+    let tagList = document.getElementById("tagList").value;
+    if (tagList.trim().length > 0) {
+        // message
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const validateContent = function () {
+    let data = editor.getData();
+    if (data.trim().length > 0) {
+        // message
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const handelArticleContentImageSrc = function (editorData) {
     const apiSourceString = "/api/coundinary/sourceImage/"
     const htmlString = editorData;
     const parser = new DOMParser();
