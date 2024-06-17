178b90c61a3d49bb0f86d7fd70fecdecab360f05
leequanno1 <binquan1234@gmail.com>
2024-06-17 12:56:13 +0700

    add filter

---
+++ src/main/java/com/example/springdemo/config/FilterConfig.java
@@ -0,0 +1,17 @@
+package com.example.springdemo.config;
+import com.example.springdemo.filters.CustomFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean<CustomFilter> customFilter() {
+        FilterRegistrationBean<CustomFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new CustomFilter());
+        registrationBean.addUrlPatterns("/personal/*", "/posting/*");
+        return registrationBean;
+    }
+}
